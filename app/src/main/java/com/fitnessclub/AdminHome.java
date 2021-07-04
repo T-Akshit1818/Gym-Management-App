b72d13145a228b5e5db10a6ec3f2c7b4ee2eb5eb
prashanth pathi <prashanthpathi88@gmail.com>
2021-07-04 14:42:28 -0400

    Admin home complete

--- app/src/main/java/com/fitnessclub/AdminHome.java
+++ app/src/main/java/com/fitnessclub/AdminHome.java
@@ -36,6 +36,38 @@ public class AdminHome extends AppCompatActivity {
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
         navigationView.setCheckedItem(R.id.nav_users);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.nav_users:
+                        navigationView.setCheckedItem(R.id.nav_users);
+                        Intent intent1 =new Intent(AdminHome.this, UserList.class);
+                        intent1.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                        startActivity(intent1);
+                        break;
+                    case R.id.nav_trainers:
+                        Intent i =new Intent(AdminHome.this, TrainersList.class);
+                        i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                        startActivity(i);
+                        break;
+                    case R.id.log_out:
+                        navigationView.setCheckedItem(R.id.log_out);
+                        Intent intent2 =new Intent(AdminHome.this,LoginActivity.class);
+                        intent2.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        SharedPreferences sharedPreferences = getSharedPreferences(Utils.SHREF, Context.MODE_PRIVATE);
+                        SharedPreferences.Editor et=sharedPreferences.edit();
+                        et.clear();
+                        et.apply();
+                        startActivity(intent2);
+
+                        break;
+
+                }
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
 
     }
 }
