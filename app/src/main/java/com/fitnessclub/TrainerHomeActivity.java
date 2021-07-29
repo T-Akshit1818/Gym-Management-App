26de484b5eefaf9e31db2ca53e9282e5ebfdb9e4
jatin <risheek.raju.n@gmail.com>
2021-07-29 11:23:11 -0400

    trainerhomeactivity java

---
+++ app/src/main/java/com/fitnessclub/TrainerHomeActivity.java
@@ -0,0 +1,116 @@
+package com.fitnessclub;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class TrainerHomeActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    CardView cdaddworkout,cdbookings,cdprogress,cdchat;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trainer_home);
+
+
+        cdaddworkout=(CardView)findViewById(R.id.cdaddworkout);
+        cdaddworkout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(TrainerHomeActivity.this,TrainerWorkoutListActivity.class);
+                startActivity(i);
+
+            }
+        });
+        cdbookings=(CardView)findViewById(R.id.cdbookings);
+        cdbookings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        cdprogress=(CardView)findViewById(R.id.cdprogress);
+        cdprogress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        cdchat=(CardView)findViewById(R.id.cdchat);
+        cdchat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout=findViewById(R.id.drawe_layout);
+        navigationView=findViewById(R.id.nav_view);
+        ActionBarDrawerToggle toggle=new
+                ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setCheckedItem(R.id.nav_home);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.nav_home:
+                        Intent intent =new Intent(TrainerHomeActivity.this,TrainerHomeActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                        startActivity(intent);
+
+                        break;
+                    case R.id.nav_profile:
+                        navigationView.setCheckedItem(R.id.nav_profile);
+                        Intent intent1 =new Intent(TrainerHomeActivity.this,TrainerProfileActivity.class);
+                        intent1.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                        startActivity(intent1);
+
+                        break;
+
+                    case R.id.nav_addwork:
+                        navigationView.setCheckedItem(R.id.nav_addwork);
+                        Intent i =new Intent(TrainerHomeActivity.this,AddWorkoutActivity.class);
+                        i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                        startActivity(i);
+
+                        break;
+                    case R.id.log_out:
+                        navigationView.setCheckedItem(R.id.log_out);
+                        Intent intent2 =new Intent(TrainerHomeActivity.this,LoginActivity.class);
+                        intent2.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        SharedPreferences sharedPreferences = getSharedPreferences(Utils.SHREF, Context.MODE_PRIVATE);
+                        SharedPreferences.Editor et=sharedPreferences.edit();
+                        et.clear();
+                        et.apply();
+                        startActivity(intent2);
+
+                        break;
+
+                }
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+    }
+}
