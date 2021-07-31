56e5528cad4e41953b71e53e25e466baa1be5ca9
jatin <risheek.raju.n@gmail.com>
2021-07-31 14:14:05 -0400

    Userlist and LoginActivity updated

--- app/src/main/java/com/fitnessclub/Admin/UserList.java
+++ app/src/main/java/com/fitnessclub/Admin/UserList.java
@@ -3,7 +3,6 @@ package com.fitnessclub.Admin;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -24,33 +23,33 @@ import java.util.List;
 
 public class UserList extends AppCompatActivity {
     ListView list;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_list);
-        list = (ListView) findViewById(R.id.list);
+        list=(ListView)findViewById(R.id.list);
         getSupportActionBar().setTitle("Users List");
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-        Call<List<UsersPojo>> reg = ServiceGenerator.getGymApi().getuserslist();
+        Call<List<UsersPojo>> reg= ServiceGenerator.getGymApi().getuserslist();
         reg.enqueue(new Callback<List<UsersPojo>>() {
             @Override
             public void onResponse(Call<List<UsersPojo>> call, Response<List<UsersPojo>> response) {
-                List<UsersPojo> usersPojos = response.body();
-                Log.d("test123", usersPojos.get(0).getEmail());
-                AdminUsersAdapter adminUsersAdapter = new AdminUsersAdapter(usersPojos,UserList.this);
+                List<UsersPojo> usersPojos=response.body();
+                Log.d("test123",usersPojos.get(0).getEmail());
+                AdminUsersAdapter adminUsersAdapter=new AdminUsersAdapter(usersPojos,UserList.this);
                 list.setAdapter(adminUsersAdapter);
             }
 
             @Override
             public void onFailure(Call<List<UsersPojo>> call, Throwable t) {
-                Log.d("test123", t.getMessage());
+                Log.d("test123",t.getMessage().toString());
             }
         });
+
 
 
     }
