1148504c2e1b34c865ce0ed5ec5808b67f0ce636
SaiTeja <saaatejgalaxy@gmail.com>
2021-07-31 15:12:36 -0400

    profile activity

--- app/src/main/java/com/fitnessclub/ProfileActivity.java
+++ app/src/main/java/com/fitnessclub/ProfileActivity.java
@@ -1,38 +1,45 @@
 package com.fitnessclub;
 
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.EditText;
-import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.fitnessclub.Model.ProfilePojo;
-import com.fitnessclub.Networking.ServiceGenerator;
-
-import java.util.List;
-
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.fitnessclub.Model.ProfilePojo;
+import com.fitnessclub.Model.SuccessOrFailureResponse;
+import com.fitnessclub.Networking.ServiceGenerator;
+
+import java.util.List;
+
 public class ProfileActivity extends AppCompatActivity {
     EditText email, password,phonenumber,fullname;
+    Button update;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        getSupportActionBar().setTitle("Profile");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         fullname=findViewById(R.id.fullname);
         email=findViewById(R.id.email);
         phonenumber=findViewById(R.id.phonenumber);
         password=findViewById(R.id.password);
-       SharedPreferences sharedPreferences = getSharedPreferences(Utils.SHREF, Context.MODE_PRIVATE);
+        SharedPreferences sharedPreferences = getSharedPreferences(Utils.SHREF, Context.MODE_PRIVATE);
         String emailId = sharedPreferences.getString("user_name", "def-val");
+
 
         Call<List<ProfilePojo>> reg= ServiceGenerator.getGymApi().profile(emailId);
         reg.enqueue(new Callback<List<ProfilePojo>>() {
@@ -52,6 +59,27 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
+        update=(Button)findViewById(R.id.update);
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Call<SuccessOrFailureResponse> reg= ServiceGenerator.getGymApi().updateprofile(fullname.getText().toString(),email.getText().toString(),password.getText().toString(),phonenumber.getText().toString());
+                reg.enqueue(new Callback<SuccessOrFailureResponse>() {
+                    @Override
+                    public void onResponse(Call<SuccessOrFailureResponse> call, Response<SuccessOrFailureResponse> response) {
+                        Intent i = new Intent(ProfileActivity.this, User_HomeActivity.class);
+                        startActivity(i);
+                    }
+
+                    @Override
+                    public void onFailure(Call<SuccessOrFailureResponse> call, Throwable t) {
+                        Toast.makeText(ProfileActivity.this,"fail",Toast.LENGTH_LONG).show();
+                        Log.d("test",t.getMessage());
+                    }
+                });
+            }
+        });
+
 
     }
     @Override
@@ -62,8 +90,5 @@ public class ProfileActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
-
-
-
     }
 }
