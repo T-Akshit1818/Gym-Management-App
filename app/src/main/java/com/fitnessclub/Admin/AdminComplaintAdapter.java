9fd94c506418ea68af52cded0c2c292116ead811
Pathi1234 <prashanthpathi88@gmail.com>
2021-08-12 14:33:11 -0400

    ADMIN  COMPLAIN

---
+++ app/src/main/java/com/fitnessclub/Admin/AdminComplaintAdapter.java
@@ -0,0 +1,57 @@
+package com.fitnessclub.Admin;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.fitnessclub.Model.ComplaintsPojo;
+import com.fitnessclub.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdminComplaintAdapter extends BaseAdapter {
+    List<ComplaintsPojo> usersPojoList=new ArrayList<>();
+    Context con;
+    String id;
+    ProgressDialog progressDialog;
+    AdminComplaintAdapter(List<ComplaintsPojo> usersPojoList, Context con){
+        this.usersPojoList=usersPojoList;
+        this.con = con;
+    }
+    @Override
+    public int getCount() {
+        return usersPojoList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.admincomplaintlist,null);
+        TextView name=v.findViewById(R.id.name);
+        TextView email=v.findViewById(R.id.email);
+        TextView type=v.findViewById(R.id.type);
+        TextView msg=v.findViewById(R.id.msg);
+
+
+        name.setText(usersPojoList.get(position).getFullname());
+        email.setText(usersPojoList.get(position).getEmail());
+        type.setText(usersPojoList.get(position).getType());
+        msg.setText(usersPojoList.get(position).getMsg());
+        return v;
+    }
+
+}
