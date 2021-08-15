b1dbbb3fb1d60be211343af007af0e2cb0406cae
Pathi1234 <prashanthpathi88@gmail.com>
2021-08-15 12:45:41 -0400

    User message activity

--- app/src/main/java/com/fitnessclub/Admin/AdminComplaintAdapter.java
+++ app/src/main/java/com/fitnessclub/Admin/AdminComplaintAdapter.java
@@ -2,17 +2,28 @@ package com.fitnessclub.Admin;
 
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.fitnessclub.AdminHome;
 import com.fitnessclub.Model.ComplaintsPojo;
+import com.fitnessclub.Model.SuccessOrFailureResponse;
+import com.fitnessclub.Model.TrainersPojo;
+import com.fitnessclub.Networking.ServiceGenerator;
 import com.fitnessclub.R;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class AdminComplaintAdapter extends BaseAdapter {
     List<ComplaintsPojo> usersPojoList=new ArrayList<>();
