2b92ffbe1fdb250edf3f1bae250b2c0933a0b447
GouthamDevineni <devinenigouthamchowdary@gmail.com>
2021-08-15 22:55:17 -0400

    forgot user activity updated code

--- app/src/main/java/com/fitnessclub/ForgotuserActivity.java
+++ app/src/main/java/com/fitnessclub/ForgotuserActivity.java
@@ -1,15 +1,85 @@
 package com.fitnessclub;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.fitnessclub.Model.SuccessOrFailureResponse;
+import com.fitnessclub.Networking.ServiceGenerator;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ForgotuserActivity extends AppCompatActivity {
-
+    EditText textEmailAddress;
+    Button btngetpassword;
+    SharedPreferences sharedPreferences;
+    String email;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgotuser);
+        getSupportActionBar().setTitle("Retrive Password");
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        sharedPreferences = getSharedPreferences(Utils.SHREF, Context.MODE_PRIVATE);
+        email=sharedPreferences.getString("user_name","");
 
+        btngetpassword=(Button)findViewById(R.id.btngetpassword);
+        textEmailAddress=(EditText)findViewById(R.id.textEmailAddress);
+        btngetpassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(textEmailAddress.getText().toString().isEmpty()) {
+                    Toast.makeText(getApplicationContext(),"Enter your Email",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                else
+                {
+                    Call<SuccessOrFailureResponse> reg= ServiceGenerator.getGymApi().forgotuser(textEmailAddress.getText().toString());
+                    reg.enqueue(new Callback<SuccessOrFailureResponse>() {
+                        @Override
+                        public void onResponse(Call<SuccessOrFailureResponse> call, Response<SuccessOrFailureResponse> response) {
+                            if (response.body().getStatus().equals("true")) {
+                                Intent s=new Intent(ForgotuserActivity.this,LoginActivity.class);
+                                startActivity(s);
+                                Toast.makeText(ForgotuserActivity.this, response.body().getMessage(), Toast.LENGTH_LONG).show();
+
+                            } else {
+                                Toast.makeText(ForgotuserActivity.this, response.body().getMessage(), Toast.LENGTH_LONG).show();
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<SuccessOrFailureResponse> call, Throwable t) {
+                            Toast.makeText(ForgotuserActivity.this, t.toString(), Toast.LENGTH_LONG).show();
+
+                        }
+                    });
+                }
+
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
